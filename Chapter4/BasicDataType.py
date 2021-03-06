ef143e51dbb72f6167c0d26bf38f63fdb4877072
kwhong95 <kwhong95@kakao.com>
2021-03-06 16:41:55 +0900

    [Chater4] Python Basic - Basic Date Type

---
+++ Chapter4/BasicDataType.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# 정수
+a = 123
+
+# 실수 
+a = 12.34
+
+# 복소수 
+a = 1 + 2j
+print(a.real)
+print(a.imag)
+print(a.conjugate())
+print(abs(a))
+
+# 8진수
+a = 0o12
+print(a)
+
+# 16진수
+a = 0x12A
+print(a)
+
+# 논리형 - Bol
+b = True
+print(b)
+
+# 숫자 연산
+
+a = 3
+b = 4
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+# 지수승 연산
+print(a ** b)
+print(a % b)
+# 나눗셈 몫 소숫점 버림
+print(a // b)
