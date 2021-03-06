3f003aafd267e5b5e68eb96f25ac2468dd30a20e
kwhong95 <kwhong95@kakao.com>
2021-03-06 16:57:12 +0900

    [Chater4] Python Basic - String Data Type

--- Chapter4/StringDataType.py
+++ Chapter4/StringDataType.py
@@ -24,3 +24,44 @@ print(a[19:])
 print(a[:3])
 print(a[:])
 print(a[7 : -11])
+
+# 문자 개수 계산 ( count() )
+a = "Python"
+print(a.count("p"))
+
+# 문자 위치 확인( find(), index() )
+a.find('y')
+a.find('p')  # 없으면 -1 출력
+a.index('y')
+# a.index('p')  ValueError : substring not found
+
+# 문자 삽입 ( join() )
+b = ','
+c = b.join('Abcd')
+print(c)
+
+# 대문자 변환
+print(a.upper())
+
+# 소문자 변환
+print(a.lower())
+
+# 공백 제거
+d = "       py       "
+print(d.lstrip())
+print(d.rstrip())
+print(d.strip())
+
+# 문자열 수정 (불가능)
+a = 'Pithon'
+# a[1] = "y" Type Error: 'str' object does not support item assignment
+
+# 문자열 바꾸기
+a = 'Python is difficult.'
+print(a.replace('difficult', 'funny'))
+
+# 문자열 나누기
+print(a.split())
+
+b = 'a, b, c, d'
+print(b.split(','))
