29190ec5bc54468488e3de2f740908b018cb0efb
kwhong95 <kwhong95@kakao.com>
2021-04-19 10:07:49 +0900

    Making Changes to the data and Saving to CSV File

--- project/pandas/analysis.py
+++ project/pandas/analysis.py
@@ -1,6 +1,14 @@
 import pandas as pd
 
-poke = pd.read_csv('pokemon_data.csv')
+df = pd.read_csv('pokemon_data.csv')
 
-print(poke.tail(5))
+# 상단 3개의 데이터 출력
+print(df.head(3))
 
+# 하단 3개의 데이터 출력
+print(df.tail(3))
+
+# 엑셀 파일 읽기
+
+df_xslx = pd.read_excel('pokemon_data.xlsx')
+print(df_xslx.haed(3))
